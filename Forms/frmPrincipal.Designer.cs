fd8cd754d5272709d75c0a29d1dd804d22c0c7a5
Renato Affonso <renato.affonso@hotmail.com>
2024-04-14 20:33:15 -0300

    Adicionar arquivos de projeto.

---
+++ Forms/frmPrincipal.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Compactor.Screens
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtCaminho = new System.Windows.Forms.TextBox();
+            this.lblTitluloCaminho = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.ltbArquivos = new System.Windows.Forms.ListBox();
+            this.chkUnico = new System.Windows.Forms.CheckBox();
+            this.chkDeletaOriginal = new System.Windows.Forms.CheckBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblQtd = new System.Windows.Forms.Label();
+            this.lblProcessando = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.Image")));
+            this.btnBuscar.Location = new System.Drawing.Point(700, 68);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(37, 33);
+            this.btnBuscar.TabIndex = 7;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtCaminho
+            // 
+            this.txtCaminho.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCaminho.Enabled = false;
+            this.txtCaminho.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCaminho.Location = new System.Drawing.Point(280, 74);
+            this.txtCaminho.Name = "txtCaminho";
+            this.txtCaminho.Size = new System.Drawing.Size(414, 23);
+            this.txtCaminho.TabIndex = 6;
+            // 
+            // lblTitluloCaminho
+            // 
+            this.lblTitluloCaminho.AutoSize = true;
+            this.lblTitluloCaminho.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitluloCaminho.ForeColor = System.Drawing.Color.White;
+            this.lblTitluloCaminho.Location = new System.Drawing.Point(28, 74);
+            this.lblTitluloCaminho.Name = "lblTitluloCaminho";
+            this.lblTitluloCaminho.Size = new System.Drawing.Size(246, 21);
+            this.lblTitluloCaminho.TabIndex = 5;
+            this.lblTitluloCaminho.Text = "Localize a pasta com os arquivos";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(12, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(234, 65);
+            this.lblTitulo.TabIndex = 8;
+            this.lblTitulo.Text = "Compactador";
+            // 
+            // pcbLogo
+            // 
+            this.pcbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pcbLogo.Image")));
+            this.pcbLogo.Location = new System.Drawing.Point(333, 12);
+            this.pcbLogo.Name = "pcbLogo";
+            this.pcbLogo.Size = new System.Drawing.Size(82, 56);
+            this.pcbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbLogo.TabIndex = 9;
+            this.pcbLogo.TabStop = false;
+            // 
+            // ltbArquivos
+            // 
+            this.ltbArquivos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ltbArquivos.FormattingEnabled = true;
+            this.ltbArquivos.Location = new System.Drawing.Point(32, 107);
+            this.ltbArquivos.Name = "ltbArquivos";
+            this.ltbArquivos.Size = new System.Drawing.Size(706, 147);
+            this.ltbArquivos.TabIndex = 10;
+            // 
+            // chkUnico
+            // 
+            this.chkUnico.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkUnico.AutoSize = true;
+            this.chkUnico.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkUnico.ForeColor = System.Drawing.Color.White;
+            this.chkUnico.Location = new System.Drawing.Point(32, 291);
+            this.chkUnico.Name = "chkUnico";
+            this.chkUnico.Size = new System.Drawing.Size(102, 19);
+            this.chkUnico.TabIndex = 11;
+            this.chkUnico.Text = "Arquivo Único";
+            this.chkUnico.UseVisualStyleBackColor = true;
+            this.chkUnico.Visible = false;
+            // 
+            // chkDeletaOriginal
+            // 
+            this.chkDeletaOriginal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkDeletaOriginal.AutoSize = true;
+            this.chkDeletaOriginal.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkDeletaOriginal.ForeColor = System.Drawing.Color.White;
+            this.chkDeletaOriginal.Location = new System.Drawing.Point(32, 316);
+            this.chkDeletaOriginal.Name = "chkDeletaOriginal";
+            this.chkDeletaOriginal.Size = new System.Drawing.Size(160, 19);
+            this.chkDeletaOriginal.TabIndex = 13;
+            this.chkDeletaOriginal.Text = "Apagar arquivos originais";
+            this.chkDeletaOriginal.UseVisualStyleBackColor = true;
+            this.chkDeletaOriginal.Visible = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIniciar.BackColor = System.Drawing.Color.White;
+            this.btnIniciar.Enabled = false;
+            this.btnIniciar.FlatAppearance.BorderSize = 2;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(541, 277);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(197, 33);
+            this.btnIniciar.TabIndex = 14;
+            this.btnIniciar.Text = "Processar";
+            this.btnIniciar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.FlatAppearance.BorderSize = 2;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(540, 316);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(197, 33);
+            this.btnLimpar.TabIndex = 15;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Image = ((System.Drawing.Image)(resources.GetObject("btnSair.Image")));
+            this.btnSair.Location = new System.Drawing.Point(692, 9);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(46, 46);
+            this.btnSair.TabIndex = 16;
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblQtd
+            // 
+            this.lblQtd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblQtd.AutoSize = true;
+            this.lblQtd.BackColor = System.Drawing.Color.Transparent;
+            this.lblQtd.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQtd.ForeColor = System.Drawing.Color.White;
+            this.lblQtd.Location = new System.Drawing.Point(28, 257);
+            this.lblQtd.Name = "lblQtd";
+            this.lblQtd.Size = new System.Drawing.Size(62, 21);
+            this.lblQtd.TabIndex = 17;
+            this.lblQtd.Text = "Total: 0";
+            // 
+            // lblProcessando
+            // 
+            this.lblProcessando.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblProcessando.AutoSize = true;
+            this.lblProcessando.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold);
+            this.lblProcessando.ForeColor = System.Drawing.Color.White;
+            this.lblProcessando.Location = new System.Drawing.Point(474, 25);
+            this.lblProcessando.Name = "lblProcessando";
+            this.lblProcessando.Size = new System.Drawing.Size(152, 30);
+            this.lblProcessando.TabIndex = 18;
+            this.lblProcessando.Text = "Processando...";
+            this.lblProcessando.Visible = false;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(749, 365);
+            this.Controls.Add(this.lblProcessando);
+            this.Controls.Add(this.lblQtd);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.chkDeletaOriginal);
+            this.Controls.Add(this.chkUnico);
+            this.Controls.Add(this.ltbArquivos);
+            this.Controls.Add(this.pcbLogo);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtCaminho);
+            this.Controls.Add(this.lblTitluloCaminho);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Compactador";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtCaminho;
+        private System.Windows.Forms.Label lblTitluloCaminho;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox pcbLogo;
+        private System.Windows.Forms.ListBox ltbArquivos;
+        private System.Windows.Forms.CheckBox chkUnico;
+        private System.Windows.Forms.CheckBox chkDeletaOriginal;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblQtd;
+        private System.Windows.Forms.Label lblProcessando;
+    }
+}
---
+++ Forms/frmPrincipal.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace Compactor.Screens
+{
+    public partial class frmPrincipal : Form
+    {
+        #region ..:: Variaveis ::..
+
+        private FileInfo[] arquivo;
+        private string fileName;
+
+        #endregion
+
+        #region ..:: Construtor ::..
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region ..:: Eventos ::.. 
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            BuscaPasta();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (txtCaminho.Text.Trim().Length < 0)
+                return;
+
+            if (MessageBox.Show("Deseja realmente compactar os arquivos deste diretório?", "AVISO!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                DesativaComponentes(labelProcessando: true, iniciar: false, formEnable: false);
+
+                ZipaPasta(txtCaminho.Text, $"{txtCaminho.Text}.zip");
+
+                MessageBox.Show("Pasta compactada", "AVISO!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Cursor.Current = Cursors.Default;
+                AtivaComponentes(labelProcessando: false, iniciar: true, formEnable: true);
+                LimparCampos();
+            }
+        }
+
+        #endregion
+
+        #region ..:: Métodos ::..
+        private void BuscaPasta()
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() != DialogResult.OK)
+                return;
+
+            txtCaminho.Text = folderBrowser.SelectedPath;
+
+            ltbArquivos.Items.Clear();
+            DirectoryInfo dir = new DirectoryInfo(txtCaminho.Text.Trim());
+            arquivo = dir.GetFiles("*", SearchOption.TopDirectoryOnly);
+            foreach (FileInfo item in arquivo)
+            {
+                fileName = item.FullName.Replace(dir.FullName, "").Remove(0, 1);
+                ltbArquivos.Items.Add(fileName);
+                lblQtd.Text = "Total: " + ltbArquivos.Items.Count;
+            }
+
+            btnIniciar.Enabled = ltbArquivos.Items.Count > 0;
+        }
+
+        private void DesativaComponentes(bool labelProcessando, bool iniciar, bool formEnable)
+        {
+            Invoke(new Action(() =>
+            {
+                lblProcessando.Visible = labelProcessando;
+                btnIniciar.Enabled = iniciar;
+                this.Enabled = formEnable;
+            }));
+        }
+
+        private void AtivaComponentes(bool labelProcessando, bool iniciar, bool formEnable)
+        {
+            Invoke(new Action(() =>
+            {
+                lblProcessando.Visible = labelProcessando;
+                btnIniciar.Enabled = iniciar;
+                this.Enabled = formEnable;
+            }));
+        }
+
+        private void ZipaPasta(string caminhoPasta, string destinationPasta)
+        {
+            using (FileStream zip = new FileStream(destinationPasta, FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(zip, ZipArchiveMode.Create))
+                {
+                    foreach (string filePath in Directory.GetFiles(caminhoPasta, "*.*", SearchOption.AllDirectories))
+                    {
+                        string entryName = filePath.Substring(caminhoPasta.Length + 1);
+                        ZipArchiveEntry entry = archive.CreateEntryFromFile(filePath, entryName);
+                    }
+                }
+            }
+        }
+
+        private void LimparCampos()
+        {
+            ltbArquivos.Items.Clear();
+            txtCaminho.Clear();
+            chkDeletaOriginal.Checked = false;
+            chkUnico.Checked = false;
+            lblQtd.Text = "0";
+        }
+
+        #endregion
+    }
+}
